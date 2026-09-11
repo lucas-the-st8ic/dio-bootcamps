e6286baed6e7607807e97e651ac1d7aac2b6b92f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-11 09:57:34 -0300

    Update Main.java

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -6,13 +6,22 @@ public class Main {
         /*Meu comentário de várias linhas
         Ipsum lorem
         */
-        /**Java doc comentário**/
+/*        *//**Java doc comentário**//*
         Scanner input = new Scanner(System.in);
         System.out.print("Olá! Qual seu nome? ");
         String name = input.next();
         System.out.print("Qual sua idade? ");
         int age = input.nextInt();
         System.out.println("Olá " +name+ " sua idade é " +
-                age+ " anos.");
+                age+ " anos.");*/
+
+        short numberShort = 1;
+        int numberInt = 1;
+        long numberLong = 1L;
+        double numberDouble = 1.0;
+        float numberFloat = 1.0F;
+        char character = 'a';
+        boolean bool = true;
+        
     }
 }
