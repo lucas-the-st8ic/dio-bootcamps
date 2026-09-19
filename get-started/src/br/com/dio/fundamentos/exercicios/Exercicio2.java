fff53e3fc987d4e196dc2bad1ef4c870462b21a9
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-19 07:30:27 -0300

    Update Exercicio2.java

--- get-started/src/br/com/dio/fundamentos/exercicios/Exercicio2.java
+++ get-started/src/br/com/dio/fundamentos/exercicios/Exercicio2.java
@@ -10,6 +10,14 @@ public class Exercicio2 {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double side;
+        double area;
+
         System.out.print("Informe o tamanho de um dos lados de um quadrado: ");
+        side = input.nextDouble();
+
+        area = side * side;
+
+        System.out.println("A área do quadrado é: " + area);
     }
 }
